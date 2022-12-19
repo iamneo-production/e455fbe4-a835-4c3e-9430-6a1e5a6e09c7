ad77318d3fbad2c0e3a391f334740a71f2d934c9
mathanNeoStark99 <mathankumar@iamneo.ai>
2022-12-19 15:58:44 +0000

    new main

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  title = 'Directives';
+
+  normalButton = true;
+  oneClickButton = false;
+  errorButton = false;
+
+  onItemChange(item:any){
+    const value = item.value;
+    if(value == 1){
+      this.normalButton = true;
+      this.oneClickButton = false;
+      this.errorButton = false;
+    }else if(value == 2){
+      this.oneClickButton = true;
+      this.normalButton = false;
+      this.errorButton = false;
+    }else{
+      this.errorButton = true;
+      this.oneClickButton = false;
+      this.normalButton = false;
+    }
+  }
+
+
 }
